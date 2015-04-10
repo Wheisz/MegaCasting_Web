d1ec8dc799aacb9818b4c73fe505693dab8a4210
S. Theodore <theodore.supiot@gmail.com>
2015-04-10 16:37:35 +0200

    Add script insert
    insert
    - domaines
    - sexes

---
+++ MegaCasting_ScriptINSERT.sql
@@ -0,0 +1,18 @@
+use MegaCastingCL
+
+insert into Domaine (Libelle)
+values
+	('CINEMA'),
+	('DANSE'),
+	('DIVERS'),
+	('ENFANT'),
+	('MODE-PHOTO'),
+	('MUSIQUE'),
+	('THEATRE'),
+	('TV-RADIO');
+
+insert into Sexe (Libelle)
+values
+	('Masculin'),
+	('Féminin'),
+	('Autre');
